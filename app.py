7270aad893080c894001e53be0f7d25570a4118e
mehmet145-web <mo75952@gmail.com>
2026-05-21 23:05:02 +0300

    Use Supabase for user login

--- app.py
+++ app.py
@@ -400,17 +400,16 @@ if st.session_state.results is not None:
             """,
             unsafe_allow_html=True
         )
-        if st.button(
-          "❤️ Favoriye ekle",
-          key=f"fav_{i}_{row['track_name']}"
-        ):
+        if st.button("❤️ Favoriye ekle", key=f"fav_{i}_{row['track_name']}"):
 
-          supabase.table("favorites").insert({
-          "username": st.session_state.username,
-          "track_name": row["track_name"],
-          "artists": row["artists"],
-          "track_genre": row["track_genre"]
-         }).execute()
+         supabase.table("favorites").insert({
+         "username": st.session_state.username,
+         "track_name": row["track_name"],
+         "artists": row["artists"],
+         "track_genre": row["track_genre"]
+        }).execute()
+
+    st.toast("Favoriye eklendi! ❤️")
 
 st.sidebar.subheader("❤️ Playlist")
 
